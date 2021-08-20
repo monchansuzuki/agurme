cf5d0996d515f8b7adaf30de03e28dd8ff18bb56
tamon-persol <60681425+tamon-persol@users.noreply.github.com>
2021-08-20 18:38:40 +0900

    remove transform

--- agurme-front/gatsby-config.js
+++ agurme-front/gatsby-config.js
@@ -15,14 +15,6 @@ module.exports = {
                 path: `${__dirname}/src/images`,
             },
         },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `markdown-pages`,
-                path: `${__dirname}/src/markdown-pages`,
-            },
-        },
-        `gatsby-transformer-remark`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         {
